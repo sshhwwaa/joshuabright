24342a6433b192eb32132a895ff2249bf8be9873
sshhwwaa <78267605+sshhwwaa@users.noreply.github.com>
2021-01-30 22:57:40 +0000

    Update googlemap.js

--- src/elements/contact/googlemap.js
+++ src/elements/contact/googlemap.js
@@ -5,7 +5,7 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class Googlemap extends Component {
     static defaultProps = {
         center: {
-          lat: 2.18,
+          lat: -2.18,
           lng: 51.43
         },
         zoom: 11
