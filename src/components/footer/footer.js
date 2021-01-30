889dc482052f0c173706a6ddd049b5ab33ab2fe1
sshhwwaa <78267605+sshhwwaa@users.noreply.github.com>
2021-01-30 19:29:35 +0000

    Add files via upload

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
 import React, {useState , useEffect} from 'react';
-import { FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram, FaSkype, FaVimeo } from "react-icons/fa";
+import { FaLinkedinIn, FaSkype } from "react-icons/fa";
 
 
 const Footer = ({showBelow}) => {
@@ -30,17 +30,17 @@ const Footer = ({showBelow}) => {
                     <div className="row">
                         <div className="col-lg-3 col-md-6 col-sm-6 col-12 mt_md--30 mt_sm--30 wow fadeInDown" data-wow-delay="200ms" data-wow-duration="0.1s">
                             <div className="ft-info">
-                                <p>WAXON <br />PORTFOLIO TEMPLATE</p>
+                                <p>Joshua Bright <br />PORTFOLIO 2021</p>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 col-sm-6 col-12 mt_md--30 mt_sm--30 wow fadeInDown" data-wow-delay="200ms" data-wow-duration="0.2s">
                             <div className="ft-info">
-                                <p>Rainbow 103, <br />0021 New York, USA</p>
+                                <p>20 St Philip Street, <br />Corsham SN13 0FS, UK</p>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 col-sm-6 col-12 mt_md--30 mt_sm--30 wow fadeInDown" data-wow-delay="200ms" data-wow-duration="0.4s">
                             <div className="ft-info">
-                                <p>hi@waxon.com<br />+77 033 442 55 57</p>
+                                <p>hi@joshuabright.co.uk<br />+44 7427 647470</p>
                             </div>
                         </div>
 
@@ -48,10 +48,10 @@ const Footer = ({showBelow}) => {
                             <div className="ft-info">
                                 <ul className="social-share">
                                     <li><a href="#link"><FaFacebookF /></a></li>
-                                    <li><a href="#link"><FaLinkedinIn /></a></li>
+                                    <li><a href="linkedin.com/in/joshua-bright-88948791"><FaLinkedinIn /></a></li>
                                     <li><a href="#link"><FaTwitter /></a></li>
                                     <li><a href="#link"><FaInstagram /></a></li>
-                                    <li><a href="#link"><FaSkype /></a></li>
+                                    <li><a href="#joshuabright"><FaSkype /></a></li>
                                     <li><a href="#link"><FaVimeo /></a></li>
                                 </ul>
                             </div>
