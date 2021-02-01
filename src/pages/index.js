69cfc9a87f64063544e28a3a8e775521d9a65cb0
sshhwwaa <78267605+sshhwwaa@users.noreply.github.com>
2021-02-01 12:05:15 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,6 @@ import SEO from "../components/seo";
 import Banner from "../components/homedefault/banner";
 import About from "../components/homedefault/about";
 import Project from "../components/homedefault/project";
-import Testimonial from "../components/homedefault/testimonial";
-import Service from "../components/homedefault/service";
-import Brand from "../components/homedefault/brand";
-import BlogPost from "../components/blogPost";
 import Contact from "../elements/contact/contact";
 
 const Index = () => (
