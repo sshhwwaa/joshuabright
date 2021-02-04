b45cd1b7de100ae87224d936bae6d13bd31833bc
sshhwwaa <78267605+sshhwwaa@users.noreply.github.com>
2021-02-04 13:41:49 +0000

    Update banner.js

--- src/components/homedefault/banner.js
+++ src/components/homedefault/banner.js
@@ -19,7 +19,7 @@ const Banner = () => {
                     }
                 }
             },
-            file(relativePath: {eq: "images/banner/bg-image-02.jpg"}) {
+            file(relativePath: {eq: "images/banner/banner-web.jpg"}) {
                 childImageSharp {
                   fixed (quality: 100, width: 1250, height: 950) {
                     ...GatsbyImageSharpFixed
