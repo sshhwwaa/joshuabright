bcb2abac2935d9e235058bd048588fd35e9abbd2
sshhwwaa <78267605+sshhwwaa@users.noreply.github.com>
2021-03-08 11:04:34 +0000

    Update projectOne.js

--- src/elements/project/projectOne.js
+++ src/elements/project/projectOne.js
@@ -5,7 +5,7 @@ import Projectcard from "./projectcard";
 const ProjectOne = () => {
     const ProjectData = useStaticQuery(graphql`
         query ProjectDataQuery {
-            allProjectJson(limit: 6) {
+            allProjectJson(limit: 9) {
                 edges {
                   node {
                     id
